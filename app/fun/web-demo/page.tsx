c1afc3768dde52e6f2c9aca569fb69f24f14ed75
Filip Kober <koberfilip2@gmail.com>
2025-01-30 20:58:18 +0100

    implement Godot game integration with messaging and input handling

--- app/fun/web-demo/page.tsx
+++ app/fun/web-demo/page.tsx
@@ -1,28 +1,35 @@
 "use client";
-import { useEffect, useRef } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useGodot } from "@/util/hooks/useGodot";
+import { useState } from "react";
 
 export default function WebDemoPage() {
 
-    const iframeRef = useRef<HTMLIFrameElement>(null);
+    const [inputText, setInputText] = useState("");
 
-    useEffect(() => {
-        if (iframeRef.current) {
-            const iframe = iframeRef.current;
-            if(iframe.contentWindow === null) return;
-            iframe.onload = () => {
-            iframe.contentWindow!.postMessage(
-                { type: "SERVER_URL", url: window.location.origin },
-                "*"
-            );
-            };
-        }
-      }, [iframeRef]);
+    const testFn = (message: unknown, ) => {
+        const msg = message as string;
+        console.log(msg);
+    }
+
+    const { send, iframeRef, addCallback } = useGodot();
+    addCallback(testFn, "clg");
+
+    const onClick = () => {
+        send(inputText, "set_label");
+    }
 
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-center gap-2">
             <h1 className="text-5xl">Web Demo</h1>
             <p>godot game test</p>
-            <iframe src="/web-demo/web-demo.html" ref={iframeRef} className="w-1/2 h-1/2" />
+            <iframe src="/web-demo/web-demo.html" className="w-1/2 h-1/2" ref={iframeRef} />
+            <div className="flex gap-2 text-2xl">
+                <Input type="text" placeholder="message" onChange={e => setInputText(e.target.value)} />
+                <Button onClick={onClick}>Send</Button>
+            </div>
         </div>
     );
 }
