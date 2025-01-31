1cf1fcf886808c63f9bb5180363cc8c9deec5ff1
Filip Kober <koberfilip2@gmail.com>
2025-01-31 19:51:32 +0100

    fix build errors

--- util/specialEffects/Ball.ts
+++ util/specialEffects/Ball.ts
@@ -1,28 +1,27 @@
 import Color from "../Color";
+import Vector from "../Vector";
 import PhysicsObject from "./PhysicsObject";
 
 interface BallProps {
-    x: number;
-    y: number;
+    coordinates: Vector;
     width: number;
     height: number;
     color: Color;
     context: CanvasRenderingContext2D;
-    xVelocity?: number;
-    yVelocity?: number;
+    velocity: Vector;
     weight?: number;
     hasGravity?: boolean
 }
 
 class Ball extends PhysicsObject {
-    constructor({ x, y, width, height, color, context, xVelocity, yVelocity, weight, hasGravity }: BallProps) {
-        super({ x, y, width, height, color, context, xVelocity, yVelocity, hasGravity, weight });
+    constructor({ coordinates, width, height, color, context, velocity, weight, hasGravity }: BallProps) {
+        super({ coordinates, width, height, color, context, velocity, hasGravity, weight });
     }
 
     public draw() {
         this.context.fillStyle = this.color.toRGB();
         this.context.beginPath();
-        this.context.arc(this.x, this.y, this.width, 0, Math.PI * 2);
+        this.context.arc(this.coordinates.x, this.coordinates.y, this.width, 0, Math.PI * 2);
         this.context.fill();
     }
 }
